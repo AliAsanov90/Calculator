6467de67ee945302149b026d5d9de820cc373dc4
ali <aliasanov07@gmail.com>
2019-10-07 22:06:15 +0300

    add logic for different operations

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="calculator">
-    <div class="btn display">{{ current }}</div>
+    <div class="btn display">{{ current || previous }}</div>
     <div @click="clear" class="btn gray-button">AC</div>
-    <div @click="negative" class="btn gray-button">+/-</div>
-    <div class="btn gray-button">%</div>
-    <div class="btn operator">/</div> 
+    <div @click="appendNegativeSign" class="btn gray-button">+/-</div>
+    <div @click="calculatePercentage" class="btn gray-button">%</div>
+    <div @click="divide" class="btn operator">/</div> 
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div class="btn operator">x</div>
+    <div @click="multiply" class="btn operator">x</div>
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div class="btn operator">-</div>
+    <div @click="subtract" class="btn operator">-</div>
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
     <div @click="append('0')" class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="appendFloatingPoint" class="btn">.</div>
+    <div @click="getResult" class="btn operator">=</div>
   </div>
 </template>
 
@@ -28,25 +28,61 @@ export default {
   name: 'Calculator',
   data() {
     return {
-      current: '0'
+      current: '0',
+      previous: null,
+      operation: null,
     }
   },
   methods: {
     clear() {
       this.current = '0'
+      this.previous = null
     },
     append(number) {
-      if (this.current.indexOf('0') === 0) {
+      if ((this.current.indexOf('0') === 0) && (this.current.indexOf('.') === -1)) {
         this.current = this.current.substring(1)
       }
       this.current = `${this.current}${number}`
     },
-    negative() {
+    appendNegativeSign() {
+      if (this.current.charAt(0) === '0') return
+
       if (this.current.indexOf('-') === -1) {
         this.current = `-${this.current}`
       } else {
         this.current = this.current.substring(1)
       }
+    },
+    appendFloatingPoint() {
+      if (this.current.indexOf('.') === -1) {
+        this.current = `${this.current}.`
+      } 
+    },
+    calculatePercentage() {
+      this.current = this.current / 100
+    },
+    storeAndClearCurrent() {
+      this.previous = this.current
+      this.current = ''
+    },
+    divide() {
+      this.storeAndClearCurrent()
+      this.operation = (a, b) => a / b
+    },
+    multiply() {
+      this.storeAndClearCurrent()
+      this.operation = (a, b) => a * b
+    },
+    subtract() {
+      this.storeAndClearCurrent()
+      this.operation = (a, b) => a - b
+    },
+    add() {
+      this.storeAndClearCurrent()
+      this.operation = (a, b) => a + b
+    },
+    getResult() {
+      this.current = `${this.operation(parseFloat(this.previous), parseFloat(this.current))}`
     }
   }
 }
@@ -54,14 +90,14 @@ export default {
 
 <style scoped>
   .calculator {
-    width: 350px;
+    width: 310px;
     margin: 0 auto;
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    grid-auto-rows: 70px;
+    grid-auto-rows: 60px;
     grid-gap: 15px;
     padding: 20px;
-    border-radius: 10px;
+    border-radius: 5px;
     background-color: black;
   }
 
@@ -69,7 +105,7 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
-    font-size: 30px;
+    font-size: 25px;
     border: none;
     border-radius: 50%;
     color: white;
@@ -86,7 +122,7 @@ export default {
   }
 
   .display {
-    font-size: 60px;
+    font-size: 40px;
     grid-column: 1 / 5;
     justify-content: flex-end;
     padding-right: 20px;
